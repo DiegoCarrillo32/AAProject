043a9a642fdea575b273c3e50360618033987d11
kosti <dcgreenforest@gmail.com>
2022-04-04 16:21:25 -0600

    fuerza bruta file

--- AnalisisNetbeansProject/src/main/java/com/mycompany/analisisnetbeansproject/FuerzaBruta.java
+++ AnalisisNetbeansProject/src/main/java/com/mycompany/analisisnetbeansproject/FuerzaBruta.java
@@ -10,37 +10,9 @@ package com.mycompany.analisisnetbeansproject;
  */
 public class FuerzaBruta {
     
-    public int sumList(int[] arr){
-        
-        int result = 0;
-        for(int i = 0; i < arr.length; i++){
-            result+=arr[i];
-        }
-        
-        return result;
-    }
-    
-    public void BruteForceAlgorithm(int[] arr){
+
         
     
-        for(int i = 0; i < arr.length; i++){
-            
-            for(int x = 0; x < arr.length; x++){
-               
-                //si los index son diferentes entonces se hace el calculo, si no se salta la iteracion            
-                if(i != x){
-                    //busca un numreo que al sumarlo el resultado es 0
-                    if(arr[i] + arr[x] == 0){
-                        System.out.println("Se encontró un 0");
-                    }
-                }
-                
-                
-            }
-        }
-        
-        
-    }
     
     
 }
